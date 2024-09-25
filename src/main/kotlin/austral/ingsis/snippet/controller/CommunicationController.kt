67c1003c5a738d17799c3d5d22d4033f44c514e9
ezequielrickert <erickert@mail.austral.edu.ar>
2024-09-25 09:46:05 -0300

    Added .env to .gitignore, enabled communication with Parser

---
+++ src/main/kotlin/austral/ingsis/snippet/controller/CommunicationController.kt
@@ -0,0 +1,21 @@
+package austral.ingsis.snippet.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestClient
+
+@RestController
+class CommunicationController {
+    val testClient: RestClient = RestClient.builder().baseUrl("http://localhost:8081").build()
+
+    @GetMapping("/testRequestMessage")
+    fun callEndpoint(): ResponseEntity<String> {
+        return ResponseEntity.ok(testClient.get().uri("/testRespondMessage").retrieve().toEntity(String::class.java).body)
+    }
+
+    @GetMapping("/testRespondMessage")
+    fun respondMessage(): ResponseEntity<String> {
+        return ResponseEntity.ok("Greetings from Snippet!")
+    }
+}
