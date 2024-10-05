b5817638ad5d8aeaebd738e24d8563c2d28b8a2f
ezequielrickert <erickert@mail.austral.edu.ar>
2024-10-05 18:32:13 -0300

    Solved failing test, added plugins

--- src/main/kotlin/austral/ingsis/snippet/SnippetApplication.kt
+++ src/main/kotlin/austral/ingsis/snippet/SnippetApplication.kt
@@ -9,6 +9,22 @@ class SnippetApplication
 
 fun main(args: Array<String>) {
     val dotenv = Dotenv.load()
-    dotenv.entries().forEach { entry -> System.setProperty(entry.key, entry.value) }
-    runApplication<SnippetApplication>(args.toString())
+
+    val dbHost = dotenv["DB_HOST"]
+    val dbPort = dotenv["DB_PORT"]
+    val dbName = dotenv["DB_NAME"]
+    val dbUser = dotenv["DB_USER"]
+    val dbPassword = dotenv["DB_PASSWORD"]
+
+    println("DB_HOST: $dbHost")
+    println("DB_PORT: $dbPort")
+    println("DB_NAME: $dbName")
+    println("DB_USER: $dbUser")
+    println("DB_PASSWORD: $dbPassword")
+
+    System.setProperty("spring.datasource.url", "jdbc:postgresql://$dbHost:$dbPort/$dbName")
+    System.setProperty("spring.datasource.username", dbUser)
+    System.setProperty("spring.datasource.password", dbPassword)
+
+    runApplication<SnippetApplication>(*args)
 }
--- src/test/kotlin/austral/ingsis/snippet/SnippetApplicationTests.kt
+++ src/test/kotlin/austral/ingsis/snippet/SnippetApplicationTests.kt
@@ -1,15 +1,42 @@
 package austral.ingsis.snippet
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.AfterAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import org.testcontainers.containers.PostgreSQLContainer
 import kotlin.test.assertNotNull
 
 @SpringBootTest
 class SnippetApplicationTests {
     @Autowired
     private lateinit var context: ApplicationContext
+
+    companion object {
+        private val postgresContainer = PostgreSQLContainer<Nothing>("postgres:14").apply {
+            withDatabaseName("test_db")
+            withUsername("test_user")
+            withPassword("test_password")
+        }
+
+        @BeforeAll
+        @JvmStatic
+        fun startContainer() {
+            postgresContainer.start()
+            // Override the Spring datasource properties to point to the test container
+            System.setProperty("spring.datasource.url", postgresContainer.jdbcUrl)
+            System.setProperty("spring.datasource.username", postgresContainer.username)
+            System.setProperty("spring.datasource.password", postgresContainer.password)
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun stopContainer() {
+            postgresContainer.stop()
+        }
+    }
 
     @Test
     fun contextLoads() {
